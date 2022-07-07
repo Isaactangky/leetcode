2357744162059cd9e19a248d159204155f53a605
Isaactangky <75126969+Isaactangky@users.noreply.github.com>
2022-07-07 13:51:27 +0800

    Update 1319_Number_of_Operations_to_Make_Network_Connected.py

--- python/11. Graph/1319_Number_of_Operations_to_Make_Network_Connected.py
+++ python/11. Graph/1319_Number_of_Operations_to_Make_Network_Connected.py
@@ -1,4 +1,4 @@
-# bfs all the computer connected to the same network
+# dfs all the computer connected to the same network
 # mark an edge as extra if we visit the same computer again
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
@@ -12,7 +12,7 @@ class Solution:
         diff_network = 0
         extra_edges = [0]
         
-        def bfs(i, pre):
+        def dfs(i, pre):
             visited[i] = True
             for comp in link[i]:
                 # comp > i: avoid double counting the cables
@@ -24,17 +24,47 @@ class Solution:
                 if comp != pre and visited[comp] and comp > i:
                     extra_edges[0] += 1
                 elif comp != pre and not visited[comp]:
-                    bfs(comp,pre)
+                    dfs(comp,pre)
             return
         
         for i in range(n):
             if not visited[i]:
-                bfs(i, -1)
+                dfs(i, -1)
                 diff_network += 1
         # diff_network - 1 edges is needed to connect all networks
         if diff_network - 1 > extra_edges[0]:
             return -1
         return diff_network - 1
+    
+    # Modified: To connect n computers, we need at least n - 1 connections 
+    # simplyfied to a classic graph problem
+    class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+
+        if len(connections) < n - 1: return -1
+        
+        link = collections.defaultdict(list)
+        for start, end in connections:
+            link[start].append(end)
+            link[end].append(start)
+        visited = [False] * n
+        diff_network = 0
+
+        def dfs(i):
+            visited[i] = True
+            for comp in link[i]:
+                if not visited[comp]:
+                    dfs(comp)
+                    
+        for i in range(n):
+            if not visited[i]:
+                dfs(i)
+                diff_network += 1
+        #print(diff_network, extra_cables)
+        return diff_network - 1
+        
+            
+        
         
             
         
