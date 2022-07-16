d21d3dbd6d2139bbc890caaae9108a59edb196cc
Isaactangky <75126969+Isaactangky@users.noreply.github.com>
2022-07-16 13:42:55 +0800

    Update 15_3Sum.py

--- python/2. Two Pointers/15_3Sum.py
+++ python/2. Two Pointers/15_3Sum.py
@@ -1,23 +1,22 @@
-class Solution:\
+class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:\
-        nums = sorted(nums)\
-        #print(nums)\
-        res = []\
-        i = 0\
-        for i in range(len(nums) - 2):\
-            if i > 0 and nums[i] == nums[i - 1]:\
-                continue\
-            l, r = i + 1, len(nums) - 1\
-            while l < r:\
-                threeSum = nums[i] + nums[l] + nums[r]\
-                if threeSum > 0:\
-                    r -= 1\
-                elif threeSum < 0:\
-                    l += 1\
-                else:\
-                    res.append([nums[i], nums[l], nums[r]])\
-                    l += 1\
-                    while l < r and nums[l] == nums[ l - 1]:\
-                        l += 1\
-                \
-        return res}
+        nums = sorted(nums)
+        #print(nums)
+        res = []
+        i = 0
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = nums[i] + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[ l - 1]:
+                        l += 1
+        return res
